1b33158e548c5d7bbd885a7d7f539992769269c0
Duy Tran <duyvutran2303@gmail.com>
2024-11-17 02:06:15 -0500

    fix: add some optimization for 3b

--- README.md
+++ README.md
@@ -22,8 +22,14 @@ make broadcast-a
 ```sh
 make broadcast-b
 ```
+A few optimization:
+- To avoid message loops (nodes keep passing the message around), each node can check if the message is already stored 
+- During inter-broadcasting (sending message between nodes and not with clients) 
+    - A node doesn't need to send to its neighbor if the message came from that neighbor
+    - A node doesn't need to send `broadcast_ok` response if the broadcast request came from a neighbor instead of a client (can be checked by the field `msg_id` in message body)
 
 ## Side Notes
 - [Having multiple binaries in a single project](https://ieftimov.com/posts/golang-package-multiple-binaries)
     - Mainly because I didn't want to scatter these challenges in different repos
 - Multiline shell commands just means escaping the newline character (`\n`), hence the backslash `\` at the end of each shell 
+- Using `omitempty` in Go for JSON marshal/unmarshal will remove the field if it is the default value for that field's type (0 for int, "" for string, etc). So to differentiate between a field that is actually empty from a field specifically sets as the default value, make that field a pointer and check for `nil` instead. 
--- cmd/broadcast/main.go
+++ cmd/broadcast/main.go
@@ -8,88 +8,99 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-type BroadcastReqBody struct {
-	Type string `json:"type"`
-	Msg  int    `json:"message"`
+type Topology map[string][]string
+
+type ResBody struct {
+	Type     string `json:"type"`
+	Messages *[]int `json:"messages,omitempty"`
 }
 
-type BroadcastResBody struct {
-	Type string `json:"type"`
-}
+type ReqBody struct {
+	// Client fields
+	Type  string `json:"type"`
+	MsgId *int   `json:"msg_id,omitempty"`
+	// InReplyTo int    `json:"in_reply_to,omitempty"`
 
-type ReadResBody struct {
-	Type string `json:"type"`
-	Msgs []int  `json:"messages"`
-}
-
-type topology map[string][]string
-
-type TopoReqBody struct {
-	Type string   `json:"type"`
-	Topo topology `json:"topology"`
+	// Broadcast
+	Topology Topology `json:"topology,omitempty"`
+	Message  int      `json:"message,omitempty"`
 }
 
 type state struct {
-	store []int
-	topo  topology
+	store map[int]struct{}
+	topo  Topology
 	mu    sync.RWMutex
 	n     *maelstrom.Node
 }
 
 func (s *state) handleBroadcast(msg maelstrom.Message) error {
-	var body BroadcastReqBody
+	var body ReqBody
 
 	if err := json.Unmarshal(msg.Body, &body); err != nil {
 		return err
 	}
 
+	s.mu.RLock()
+	_, isMessageInStore := s.store[body.Message]
+	s.mu.RUnlock()
+	if isMessageInStore {
+		return nil
+	}
+
 	s.mu.Lock()
-	s.store = append(s.store, body.Msg)
+	s.store[body.Message] = struct{}{}
 	s.mu.Unlock()
 
 	for _, dst := range s.topo[s.n.ID()] {
-		if dst == msg.Src || dst == s.n.ID() {
+		if dst == msg.Src {
 			continue
 		}
 
-		s.n.RPC(dst, body, func(msg maelstrom.Message) error {
-			return nil
+		s.n.Send(dst, ReqBody{
+			Type:    "broadcast",
+			Message: body.Message,
 		})
 	}
 
-	return s.n.Reply(msg, BroadcastResBody{
-		Type: "broadcast_ok",
-	})
+	if body.MsgId != nil {
+		return s.n.Reply(msg, ResBody{
+			Type: "broadcast_ok",
+		})
+	}
+
+	return nil
 }
 
 func (s *state) handleRead(msg maelstrom.Message) error {
 	s.mu.RLock()
-	copyStore := make([]int, len(s.store))
-	copy(copyStore, s.store)
+	copyStore := make([]int, 0, len(s.store))
+	for k := range s.store {
+		copyStore = append(copyStore, k)
+	}
 	s.mu.RUnlock()
 
-	return s.n.Reply(msg, ReadResBody{
-		Type: "read_ok",
-		Msgs: copyStore,
+	return s.n.Reply(msg, ResBody{
+		Type:     "read_ok",
+		Messages: &copyStore,
 	})
 }
 
 func (s *state) handleTopo(msg maelstrom.Message) error {
-	var body TopoReqBody
+	var body ReqBody
 	if err := json.Unmarshal(msg.Body, &body); err != nil {
 		return err
 	}
 
-	s.topo = body.Topo
+	s.topo = body.Topology
 
-	return s.n.Reply(msg, BroadcastResBody{
+	return s.n.Reply(msg, ReqBody{
 		Type: "topology_ok",
 	})
 }
 
 func main() {
 	s := state{
-		store: []int{},
+		store: map[int]struct{}{},
 		n:     maelstrom.NewNode(),
 	}
 
