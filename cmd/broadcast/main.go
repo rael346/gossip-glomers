dc539ee7fe36fd1602fce857e5a89b9ca68d5cff
Duy Tran <duyvutran2303@gmail.com>
2024-11-16 19:37:03 -0500

    feat: challenge 3b

--- cmd/broadcast/main.go
+++ cmd/broadcast/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -21,56 +22,82 @@ type ReadResBody struct {
 	Msgs []int  `json:"messages"`
 }
 
+type topology map[string][]string
+
 type TopoReqBody struct {
-	Type     string              `json:"type"`
-	Topology map[string][]string `json:"topology"`
+	Type string   `json:"type"`
+	Topo topology `json:"topology"`
 }
 
-var store []int = []int{}
+type state struct {
+	store []int
+	topo  topology
+	mu    sync.RWMutex
+	n     *maelstrom.Node
+}
+
+func (s *state) handleBroadcast(msg maelstrom.Message) error {
+	var body BroadcastReqBody
+
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	s.mu.Lock()
+	s.store = append(s.store, body.Msg)
+	s.mu.Unlock()
+
+	for _, dst := range s.topo[s.n.ID()] {
+		if dst == msg.Src || dst == s.n.ID() {
+			continue
+		}
+
+		s.n.RPC(dst, body, func(msg maelstrom.Message) error {
+			return nil
+		})
+	}
+
+	return s.n.Reply(msg, BroadcastResBody{
+		Type: "broadcast_ok",
+	})
+}
+
+func (s *state) handleRead(msg maelstrom.Message) error {
+	s.mu.RLock()
+	copyStore := make([]int, len(s.store))
+	copy(copyStore, s.store)
+	s.mu.RUnlock()
+
+	return s.n.Reply(msg, ReadResBody{
+		Type: "read_ok",
+		Msgs: copyStore,
+	})
+}
+
+func (s *state) handleTopo(msg maelstrom.Message) error {
+	var body TopoReqBody
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	s.topo = body.Topo
+
+	return s.n.Reply(msg, BroadcastResBody{
+		Type: "topology_ok",
+	})
+}
 
 func main() {
-	n := maelstrom.NewNode()
+	s := state{
+		store: []int{},
+		n:     maelstrom.NewNode(),
+	}
 
-	n.Handle("broadcast", func(msg maelstrom.Message) error {
-		var body BroadcastReqBody
+	s.n.Handle("broadcast", s.handleBroadcast)
+	s.n.Handle("read", s.handleRead)
+	s.n.Handle("topology", s.handleTopo)
 
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		store = append(store, body.Msg)
-
-		resBody := BroadcastResBody{
-			Type: "broadcast_ok",
-		}
-
-		return n.Reply(msg, resBody)
-	})
-
-	n.Handle("read", func(msg maelstrom.Message) error {
-		resBody := ReadResBody{
-			Type: "read_ok",
-			Msgs: store,
-		}
-
-		return n.Reply(msg, resBody)
-	})
-
-	n.Handle("topology", func(msg maelstrom.Message) error {
-		var body TopoReqBody
-
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		resBody := BroadcastResBody{
-			Type: "topology_ok",
-		}
-
-		return n.Reply(msg, resBody)
-	})
-
-	if err := n.Run(); err != nil {
+	if err := s.n.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
